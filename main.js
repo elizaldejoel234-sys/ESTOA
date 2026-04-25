afe7db9e5441dd2224ca31c37bc3769ae038e75a
elizaldejoel234-sys <elizaldejoel234@gmail.com>
2026-04-25 17:17:35 -0500

    Initial commit from Aura Studio

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const status = document.getElementById('status');
+    status.innerText = 'JavaScript is active! 🚀';
+    console.log('Hello from Aura Basic Project');
+});
